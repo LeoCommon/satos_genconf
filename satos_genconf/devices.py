32daf11e5248cc8e3d202e1c6464c69ceec458cb
Martin Böh <contact@martb.dev>
2023-01-25 17:58:05 +0100

    devices: remove newline and whitespace from serial

--- satos_genconf/devices.py
+++ satos_genconf/devices.py
@@ -4,7 +4,7 @@ import uuid
 
 class Device(ABC):
     def serial(self) -> str:
-        return self._serial().replace(':','').replace('0x', '')
+        return ''.join(self._serial().replace(':','').replace('0x', ''))
 
     @abstractmethod
     def _serial() -> str:
@@ -22,18 +22,21 @@ class RPI(Device):
         return Path('/proc/device-tree/model').read_text()
 
 class RPI3(RPI):
-    # Combine eth and wifi mac because thers no unique serial number on pi3
+    # Combine eth and wifi mac because there is no unique serial number on pi3
+    ethernetMacNode = Path('/sys/class/net/eth0/address')
+    wifiMacNode = Path('/sys/class/net/wlan0/address')
+
     def _serial(self) -> str:
-        ethSerial = Path('/sys/class/net/eth0/address').read_text()
-        wifiSerial = Path('/sys/class/net/wlan0/address').read_text()
-        return ethSerial + wifiSerial
+        ethMac = self.ethernetMacNode.read_text() if self.ethernetMacNode.exists() else "FF:FF:FF:FF:FF:FF"
+        wifiMac = self.wifiMacNode.read_text() if self.wifiMacNode.exists() else "FF:FF:FF:FF:FF:FF"
+        return ethMac.strip() + wifiMac.strip()
 
     def __str__(self):
         return "RPI3Device: " + super().__str__()
 
 class RPI4(RPI):
     def _serial(self) -> str:
-        return Path('/proc/device-tree/serial-number').read_text()
+        return Path('/proc/device-tree/serial-number').read_text().strip()
 
     def __str__(self):
         return "RPI4Device: " + super().__str__()
